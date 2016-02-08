7ab4a3e27b1efb3de1d67f2710d80b847343f1e0
Tao Lin <lin.tao.5711y@gmail.com>
2016-02-08 15:40:02 -0500

    added sqrt function

---
+++ 01recursion/Recursion.java
@@ -0,0 +1,19 @@
+public class Recursion{
+	public String name(){
+		return "Lin,Tao";
+	}
+	
+	public double sqrt(double n){
+		return sqrtH(n,n/2);		
+	}
+	
+	public double sqrtH(double n, double guess){
+		if (PE(guess*guess, n) < 5) return guess;
+		double newguess = ((n/guess)+guess)/2;
+		return sqrtH(n,newguess);
+	}
+	
+	public double PE(double measured, double actual){
+		return ((actual - measured)/actual)*100;
+	}
+}
