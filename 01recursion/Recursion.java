75cbdb7a4f7af0295ea00502aaa904c135b351ce
Tao Lin <lin.tao.5711y@gmail.com>
2016-02-10 13:12:45 -0500

    making solve

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -1,21 +1,27 @@
 public class Recursion{
-	public String name(){
-		return "Lin,Tao";
-	}
+    public String name(){
+	return "Lin,Tao";
+    }
 	
-	public double sqrt(double n){
-		if (n<0) throw new IllegalArgumentException();
-		return sqrtH(n,n/2);		
-	}
+    public double sqrt(double n){
+	if (n<0) throw new IllegalArgumentException();
+	if (n==0) return 0;
+	return sqrtH(n,1);		
+    }
 	
-	public double sqrtH(double n, double guess){
-		if (PE(guess*guess, n) < 5) return guess;
-		double newguess = ((n/guess)+guess)/2;
-		return sqrtH(n,newguess);
-	}
+    private double sqrtH(double n, double guess){
+	/*
+	System.out.println(guess);
+	System.out.println(PE(guess*guess, n));
+	System.out.println("loop done");
+	*/
+	if (PE(guess*guess, n) < .0001) return guess;
+	double newguess = ((n/guess)+guess)/2;
+	return sqrtH(n,newguess);
+    }
 	
-	public double PE(double measured, double actual){
-		return ((actual - measured)/actual)*100;
-	}
+    public double PE(double measured, double actual){
+	return (Math.abs((actual - measured))/actual)*100;
+    }
 
 }
