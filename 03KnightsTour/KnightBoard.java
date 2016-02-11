079d55c4429a0a6fea4016e9f3972fa2e6b6f498
Tao Lin <lin.tao.5711y@gmail.com>
2016-02-11 13:05:20 -0500

    knights tour

---
+++ 03KnightsTour/KnightBoard.java
@@ -0,0 +1,11 @@
+public class KnightBoard{
+    private int[][]board;
+
+    public KnightBoard(int size){
+	board = new int[size][size];
+    }
+
+    public boolean solve(){}
+
+    public void printSolution(){}
+}
