e5cc419755ba8e73bde6ea918c6adf5289f403e3
Tao Lin <lin.tao.5711y@gmail.com>
2016-02-12 13:04:41 -0500

    working on solve still

---
+++ 02NQueens/QueenBoard.java
@@ -0,0 +1,79 @@
+public class QueenBoard{
+    private int[][] board;
+
+    public QueenBoard(int size){
+	board = new int[size][size];
+    }
+
+    public boolean solve(){ return solveH(0); }
+
+    private boolean solveH(int col){
+	return false;
+    }
+
+    public boolean addQueen(int row, int col){
+	if(board[row][col] != 0){
+	    return false;
+	}
+	board[row][col] = 1;
+	int offset = 1;
+	while(col+offset < board[row].length){
+	    board[row][col+offset]--;
+	    if(row - offset >= 0){
+		board[row-offset][col+offset]--;
+	    }
+	    if(row + offset < board.length){
+		board[row+offset][col+offset]--;
+	    }
+	    offset++;
+	}
+	return true;
+    }
+
+    public boolean removeQueen(int row, int col){
+	if(board[row][col] != 1){
+	    return false;
+	}
+	board[row][col] = 0;
+	int offset = 1;
+	while(col+offset < board[row].length){
+	    board[row][col+offset]++;
+	    if(row - offset >= 0){
+		board[row-offset][col+offset]++;
+	    }
+	    if(row + offset < board.length){
+		board[row+offset][col+offset]++;
+	    }
+	    offset++;
+	}
+	return true;
+    }
+
+    public String toString(){
+	String ans = "";
+	for(int r = 0; r < board.length; r++){
+	    for(int c = 0; c < board[0].length; c++){
+		ans+= board[r][c]+"\t";
+	    }
+	    ans+="\n";
+	}
+	return ans;
+    }
+
+    public void printBoard(){
+	String a = "";
+	int[][] b = board;
+	for(int r = 0; r < board.length; r++){
+	    for(int c = 0; c < board[0].length; c++){
+		if(board[r][c]== 0) a+="_  ";
+		if(board[r][c]== -1) a+="/  ";
+		if(board[r][c]== 1) a+="Q  ";
+	    }
+	    a+="\n";
+	}
+	System.out.println(a);
+    }
+
+	
+}
+
