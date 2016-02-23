7ae2af19947eb70e97f1ca6f7e73de33f0a1e88d
Tao Lin <lin.tao.5711y@gmail.com>
2016-02-23 04:27:28 -0500

    imc close

--- 03KnightsTour/KnightBoard.java
+++ 03KnightsTour/KnightBoard.java
@@ -2,10 +2,28 @@ public class KnightBoard{
     private int[][]board;
 
     public KnightBoard(int size){
-	board = new int[size][size];
+		board = new int[size][size];
     }
 
-    public boolean solve(){}
+    public boolean solve(){return solveH(1,0,0);}
+	
+	private boolean solveH(int moveNum, int x, int y){
+		
+	}
 
-    public void printSolution(){}
+	private boolean inRange(int x, int y){
+		return x>=0 && x<board.length && y>=0 && y<board.length;
+	}
+	
+    public void printSolution(){
+		String line = "";
+		for(int i = 0; i < board.length; i++){
+			for(int j = 0; j < board.length; j++){
+				if (board[i][j]<10){line += "_"+board[i][j]+" ";}
+				line += board[i][j]+" ";
+			}
+			line += "\n"
+		}
+		System.out.println(line);
+	}
 }
