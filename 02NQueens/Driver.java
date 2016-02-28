9fa1a68138be8803c0663ca40153e21f6fb8d602
Tao Lin <lin.tao.5711y@gmail.com>
2016-02-27 20:02:44 -0500

    QueenBoard is not complete

--- 02NQueens/Driver.java
+++ 02NQueens/Driver.java
@@ -1,9 +1,10 @@
 public class Driver{
     public static void main(String[]args){
-
-	QueenBoard a = new QueenBoard(4);
-
-	a.solve();
-	a.printBoard();
+		if(args.length!=0){
+			QueenBoard board = new QueenBoard(Integer.parseInt(args[0]));
+			board.solve();
+			String s = board.toString();
+			System.out.println(s);
+		}else{System.out.println("Give me a number");}
     }
 }
