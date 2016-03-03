faa8640818404982e29924bc3f9ca1d82e79c879
Tao Lin <lin.tao.5711y@gmail.com>
2016-03-03 06:16:53 -0500

    merge funtion

---
+++ 06merge/merge.java
@@ -0,0 +1,21 @@
+public class merge{
+	public static int[] merge(int[]a, int[]b){
+		int[]finalInt = new int[a.length + b.length];
+		int x = 0;
+		int y = 0;
+		int z = 0;
+		
+		while (x < a.length && y < b.length){
+			if (a[x] < b[y]){
+				finalInt[z] = a[x];
+				z++;
+				x++;
+			}else{
+				finalInt[z] = b[y];
+				z++;
+				y++;
+			}				
+		}
+		return finalInt;
+	}
+}	
