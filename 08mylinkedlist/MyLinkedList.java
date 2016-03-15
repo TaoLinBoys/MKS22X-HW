7383002f7db778038d283437ee671729dd64694a
Tao Lin <lin.tao.5711y@gmail.com>
2016-03-15 13:14:27 -0400

    work on it

--- 08mylinkedlist/MyLinkedList.java
+++ 08mylinkedlist/MyLinkedList.java
@@ -4,26 +4,47 @@ public class MyLinkedList{
 	private int val;
 	private LNode next;
 	public LNode(int newval){
-	    val = newVal;
+	    val = newval;
 	}
-	public getval()return val;
-	public getNext()return next;
-	public setval(int newval)val = newval;
-	public setNext(LNode newNext)next = newNext;	
+	public int getval(){return val;}
+	public LNode getNext(){return next;}
+	public void setval(int newval){val = newval;}
+	public void setNext(LNode newNext){next = newNext;}	
     }
 
     private LNode start;
     private int size;
     
     public boolean add(int value){
-	if(start.getNext == null){
+	if(start.getNext() == null){
 	    start = new LNode(value);
+	    return true;
 	}else{
 	    LNode current = start;
-	    while(current.getNext != null){
+	    while(current.getNext() != null){
 		current = current.getNext();
 	    }
 	    current = new LNode(value);
+	    return true;
+	}
+    }
+
+    public String toString(){
+	String list = "[";
+	LNode current = start;
+	while(current.getNext() != null){
+	    list += current.getval() + ", ";
+	}
+	list += current.getval() + "]";
+	return list;
+    }
+
+    public static void main(String[]args){
+	MyLinkedList test = new MyLinkedList();
+	for(int i = 0; i < 5; i++){
+	    int rand = (int)(Math.random()*50);
+	    test.add(rand);
+	    System.out.println(test.toString());
 	}
     }
 
