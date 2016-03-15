67bef7589de5f2cddb2a03781fa671387f648e97
Tao Lin <lin.tao.5711y@gmail.com>
2016-03-15 12:57:52 -0400

    starting linkedlists

---
+++ 08mylinkedlist/MyLinkedList.java
@@ -0,0 +1,23 @@
+public class MyLinkedList{
+
+    public class LNode{
+	private int val;
+	private LNode next;
+	public LNode(int newval){
+	    val = newVal;
+	}
+	public getval()return val;
+	public getNext()return next;
+	public setval(int newval)val = newval;
+	public setNext(LNode newNext)next = newNext;	
+    }
+
+    private LNode start;
+    private int size;
+    
+    public boolean add(int value){
+	if(start.getNext == null)
+    }
+
+
+}
