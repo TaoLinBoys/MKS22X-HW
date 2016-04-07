376bbb2a40d618a1978346760efd7d91ddcf6044
Tao Lin <lin.tao.5711y@gmail.com>
2016-04-06 22:55:39 -0400

    add works

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -3,7 +3,7 @@ public class MyDeque<T>{
     private int size, start, end;
     
     @SuppressWarnings("unchecked") public MyDeque(){
-	data = (T[]) new Object[4];
+	data = (T[]) new Object[5];
 	start = 2;
 	end = 2;
 	size = 0;
@@ -33,35 +33,53 @@ public class MyDeque<T>{
     }
     
     public void addFirst(T value){
-	//if(size == data.length()){
-	//    grow();
-	//	}
-	if(start < 0){
+	//System.out.println(start);
+	if(size == data.length){
+	    grow();
+	    start--;
+	    data[start] = value;
+	    size++;
+	}else if(size == 0){
+	    data[start] = value;
+	    size++;
+	}else if(start == 0){
 	    start = data.length-1;
 	    data[start] = value;
-	}
-	if(size == 0){
-	    data[start] = value;
+	    size++;
 	}else{
 	    start--;
 	    data[start] = value;
+	    size++;
 	}
-	size++;
     }
     
     public String toString(){
 	String dataS = "[ ";
-	for(int i = start; i <= end; i++){
-	    dataS += data[i] + " ";
+	for(int i = 0; i < data.length; i++){
+	    if(data[i] == null){
+		dataS += "_ ";
+	    }else{
+		dataS += data[i] + " ";
+	    }
 	}
-	return dataS+"]";
+	return dataS+"]"+" start: "+data[start]+" end: "+data[end];
     }
 
     public static void main(String[]args){
 	MyDeque<Integer> test = new MyDeque<Integer>();
+
+	//adding to deque a bunch of times to test add and grow
 	test.addFirst(3);
 	System.out.println(test);
-	test.addFirst(21312321);
+	test.addFirst(5);
+	System.out.println(test);
+	test.addFirst(4);
+	System.out.println(test);
+	test.addFirst(9);
+	System.out.println(test);
+	test.addFirst(30);
+	System.out.println(test);
+	test.addFirst(7);
 	System.out.println(test);
     }
 }
