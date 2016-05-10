3bc1c94b8e86f1a095470853774428ec87463d10
Tao Lin <lin.tao.5711y@gmail.com>
2016-05-10 13:15:41 -0400

    starting med

---
+++ 14median/RunningMedian.java
@@ -0,0 +1,17 @@
+public class RunningMedian{
+    MyHeap<Integer> lowerTM;
+    MyHeap<Integer> higherTM;
+    
+    public RunningMedian(){
+	lowerTM = new MyHeap<Integer>();
+	higherTM = new MyHeap<Integer>();
+    }
+
+    public double getMedian(){
+	return 1.0;
+    }
+
+    public void add(Integer x){
+	
+    }
+}
